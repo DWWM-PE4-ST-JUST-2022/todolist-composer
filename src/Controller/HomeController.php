15400ddd406641f1215850490a7ad37a523ef4c0
Benjamin Georgeault <benjamin@drosalys.fr>
2022-07-13 15:52:01 +0200

    Add routing config to the project with its home controller.

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hb\TodolistComposer\Controller;
+
+class HomeController
+{
+    /**
+     * For now, only echo a string.
+     */
+    public function __invoke()
+    {
+        echo 'Hello';
+    }
+}
