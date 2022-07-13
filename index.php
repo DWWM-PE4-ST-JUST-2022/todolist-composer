6c283ea727ddc785d3215d7cbd2d2b46ef586372
Benjamin Georgeault <benjamin@drosalys.fr>
2022-07-13 09:45:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TODO List</title>
+</head>
+<body>
+
+<ul>
+    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam at dolorem dolorum ex excepturi illum
+        ipsa minus nobis pariatur quas quidem, saepe sed tenetur ut? Eaque praesentium quia vero.
+    </li>
+    <li>Aperiam architecto consectetur dicta dolor ea esse eum in inventore, maxime nemo non officiis omnis provident
+        sequi veniam voluptas, voluptates. Minima optio perferendis qui! Deserunt in iusto quisquam. A, quos.
+    </li>
+    <li>Aliquid atque consequatur cupiditate dolores minus optio quia unde vel? Adipisci consectetur, deleniti
+        distinctio dolore eaque ex fugiat impedit labore, minus, officia officiis perferendis placeat quod recusandae
+        rem veritatis voluptas!
+    </li>
+    <li>At aut esse, eum ex laboriosam maiores non unde. Assumenda cupiditate doloribus exercitationem illo in iusto
+        maiores perferendis, quae qui rem soluta tempora ullam vitae! Dicta reprehenderit rerum tempora vero.
+    </li>
+    <li>Error esse, harum nostrum numquam quasi reprehenderit sapiente sed voluptatibus! At consectetur deleniti ea,
+        eaque illum in iusto labore necessitatibus pariatur possimus quis quo saepe sapiente tempora tenetur ullam
+        voluptate.
+    </li>
+</ul>
+
+</body>
+</html>
