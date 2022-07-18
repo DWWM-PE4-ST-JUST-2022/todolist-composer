faaa4c3afca6e1748fca81d1c36f3ec1880e0708
Benjamin Georgeault <benjamin@drosalys.fr>
2022-07-18 14:09:38 +0200

    Update class use instead of fullnamespace use in routes.

--- routes.php
+++ routes.php
@@ -2,6 +2,7 @@
 
 // Use this file to define all existing routes and corresponding controllers.
 
+use Hb\TodolistComposer\Controller\HomeController;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
@@ -11,7 +12,7 @@ $routes = new RouteCollection();
 // Declare first route (home).
 $routes->add('home', new Route('/', [
     // Config the class that will be used as route's controller.
-    '_controller' => \Hb\TodolistComposer\Controller\HomeController::class,
+    '_controller' => HomeController::class,
     // You can use the `::class` magic static property on a class to get
     // a string with the fullname of the class (namespace + it name).
 ]));
